3e0c9f7555f190c1f62d7efbee12c7e81b2cdd1c
Fatma Nur Sarı <fnursari96@gmail.com>
2023-05-30 21:27:40 +0300

    first commit

--- src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
+++ src/main/java/com/project/schoolmanagment/SchoolManagementApplication.java
@@ -9,6 +9,10 @@ public class SchoolManagementApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SchoolManagementApplication.class, args);
+
+
+
+        //first line of my code
     }
 
 }
