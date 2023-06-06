3edd289ba3311845636d62ff242e2baf6d586535
Fatma Nur Sarı <fnursari96@gmail.com>
2023-06-06 17:42:41 +0300

    V2

--- src/main/java/com/project/schoolmanagment/utils/Messages.java
+++ src/main/java/com/project/schoolmanagment/utils/Messages.java
@@ -2,4 +2,8 @@ package com.project.schoolmanagment.utils;
 
 public class Messages {
 
+    //Shortcut -> CTRL+SHIFT+U --> Turn the uppercase
+    public static final String ALREADY_SEND_A_MESSAGE_TODAY = "Error: You have already send a message with this e-mail";
+
+
 }
