e4876d1b8dbadf21d43d92e0f5a832863a33275b
Fatma Nur Sarı <fnursari96@gmail.com>
2023-06-07 19:43:47 +0300

    day3 v4 end of the day

---
+++ src/main/java/com/project/schoolmanagment/entity/concretes/Dean.java
@@ -0,0 +1,20 @@
+package com.project.schoolmanagment.entity.concretes;
+
+import com.project.schoolmanagment.entity.abstracts.User;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import net.bytebuddy.implementation.bind.annotation.Super;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+public class Dean extends User {
+
+
+}
