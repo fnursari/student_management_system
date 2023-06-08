c7f35704dd3227a8bc7d474695c4b0484d053783
Fatma Nur Sarı <fnursari96@gmail.com>
2023-06-08 18:00:16 +0300

    day4 v1

---
+++ src/main/java/com/project/schoolmanagment/entity/concretes/StudentInfo.java
@@ -0,0 +1,46 @@
+package com.project.schoolmanagment.entity.concretes;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.project.schoolmanagment.entity.enums.Note;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class StudentInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    /**
+     * number of absentees of the student in the lessons
+     */
+    private Integer absentee;
+
+    private Double midtermExam;
+
+    private Double finalExam;
+
+    private Double examAverage;
+
+    private String infoNote;
+
+    @ManyToOne
+    @JsonIgnoreProperties("teacher")
+    private Teacher teacher;
+
+    @ManyToOne
+    private Student student;
+
+    @Enumerated(EnumType.STRING)
+    private Note letterGrade;
+    // ctrl+shift+alt+l --> reformatted the code
+
+
+}
